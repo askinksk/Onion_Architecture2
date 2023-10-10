7178d6f985e00bf927d6b4af82defd46cfc20b6d
Aşkın Kesik <140115554+askinksk@users.noreply.github.com>
2023-10-10 23:01:22 +0300

    Add files via upload

---
+++ Core/OnionArcExample.Domain/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionArcExample.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int ProductId { get; set; }
+    }
+}
---
+++ Core/OnionArcExample.Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using OnionArcExample.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionArcExample.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public Int16 UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
